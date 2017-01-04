e2ad6ff54c591020cb883544b37e450441203566
David Cate <david@element-360.com>
2017-01-04 08:23:40 -0500

    correction

--- lesson_3_exercises/medium_1.rb
+++ lesson_3_exercises/medium_1.rb
@@ -2,6 +2,7 @@
 # Let's do some "ASCII Art" (a stone-age form of nerd artwork from back in the days before computers had video screens).
 
 # For this exercise, write a one-line program that creates the following output 10 times,
+"The Flintstones Rock!"
 # with the subsequent line indented 1 space to the right:
 
 10.times { |number| puts (" " * number) + "The Flintstones Rock!" }

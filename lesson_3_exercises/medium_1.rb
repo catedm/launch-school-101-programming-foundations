ca5b96bcafd087c196de81c0cbddbae28987f692
David Cate <david@element-360.com>
2017-01-04 08:23:03 -0500

    correction

--- lesson_3_exercises/medium_1.rb
+++ lesson_3_exercises/medium_1.rb
@@ -1,5 +1,4 @@
 # Question 1
-
 # Let's do some "ASCII Art" (a stone-age form of nerd artwork from back in the days before computers had video screens).
 
 # For this exercise, write a one-line program that creates the following output 10 times,

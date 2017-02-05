8493b5122472c386af76e96badbf20a02726ee8a
David Cate <david@element-360.com>
2017-02-05 15:41:48 -0500

    medium exercises

--- ruby_small_problems/medium_1/stack_machine_interpretation.rb
+++ ruby_small_problems/medium_1/stack_machine_interpretation.rb
@@ -2,25 +2,10 @@
 # register value is separate from the STACK - initialize at 0
 # all operations will be integer operations
 
-DIGITS = {'1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6,
-          '7' => 7, '8' => 8, '9' => 9, '0' => 0}
-
-def integer_array(string)
-  integer_array = string.split.map do |item|
-    if item.size == 1
-      item = DIGITS[item]
-    else
-      item
-    end
-  end
-  integer_array
-end
-
 def minilang(string)
-  command_order = integer_array(string)
   register = 0
   stack = []
-  command_order.each do |command|
+  string.split.each do |command|
     case command
     when "PUSH" then stack << register
     when "ADD" then  register += stack.pop
@@ -30,7 +15,7 @@ def minilang(string)
     when "MOD" then  register %= stack.pop
     when "POP" then  register = stack.pop
     when "PRINT" then puts register
-    else              register = command
+    else              register = command.to_i
     end
   end
 end
